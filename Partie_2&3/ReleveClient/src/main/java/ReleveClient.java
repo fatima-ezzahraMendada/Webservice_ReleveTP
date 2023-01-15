fce3337f1831c93ecdf7c72543597b70253881eb
fatima-ezzahraMendada <96340330+fatima-ezzahraMendada@users.noreply.github.com>
2023-01-15 02:31:57 +0100

    Update ReleveClient.java

--- Partie_2&3/ReleveClient/src/main/java/ReleveClient.java
+++ Partie_2&3/ReleveClient/src/main/java/ReleveClient.java
@@ -1,5 +1,11 @@
+import proxy.*;
+
 public class ReleveClient {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws JAXBException_Exception {
+        ReleveWs stub=new ReleveWebService().getReleveWsPort();
+        Releve rel=stub.getReleve();
+        System.out.println(rel.getRib());
+
 
     }
 }
